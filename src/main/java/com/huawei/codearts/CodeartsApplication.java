5415f7472d39313256f72f4c0e042a2bcdc5e9b4
SrSujeto <emilianolp@ciencias.unam.mx>
2024-12-20 14:41:12 -0600

    First commit

---
+++ src/main/java/com/huawei/codearts/CodeartsApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.codearts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huawei/codearts/CodeartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huawei.codearts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
